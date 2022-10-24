fc92fe917b360a49ed36d3c7801f1e10c7891828
Elroy Kanye <elroykanye@gmail.com>
2022-10-24 17:12:00 +0100

    add model for payment setting

---
+++ src/app/models/dto/payment-setting.model.ts
@@ -0,0 +1,9 @@
+export class PaymentSetting {
+  constructor(
+    public id: number,
+    public tuitionFee: number,
+    public applicationFee: number,
+    public schoolId: number,
+  ) {
+  }
+}
