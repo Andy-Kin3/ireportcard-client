a26dc76967ac95e1991e943fdf446a3abc928ad3
Elroy Kanye <elroykanye@gmail.com>
2022-11-07 11:10:13 +0100

    refactor academic-year.model.ts

--- src/app/models/dto/academic-year.model.ts
+++ src/app/models/dto/academic-year.model.ts
@@ -1,9 +1,23 @@
-export interface AcademicYear {
-  id: number;
+import {BaseModel} from "../base/base.model";
+
+export interface IAcademicYear {
+  id?: number;
   name: string;
   startYear: number;
-  endYear: number;
-  academicInfo: AcademicInfo
+  endYear?: number;
+  academicInfo?: AcademicInfo
+}
+
+export class AcademicYear extends BaseModel implements IAcademicYear{
+  constructor(
+    public name: string,
+    public startYear: number,
+    public schoolId: number,
+    public endYear?: number,
+    public academicInfo?: AcademicInfo,
+  ) {
+    super();
+  }
 }
 
 export class AcademicInfo {
