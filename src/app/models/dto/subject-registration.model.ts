e2e37b229a3097bb387e2971f6ca6029347db967
Elroy Kanye <elroykanye@gmail.com>
2022-11-10 11:12:14 +0100

    match section.model.ts, subject-registration.model.ts and user.model.ts to api

--- src/app/models/dto/subject-registration.model.ts
+++ src/app/models/dto/subject-registration.model.ts
@@ -1,7 +1,18 @@
+import {VersionedModel} from "../base/versioned.model";
+
 export interface SubjectRegistration {
-  id: number;
+  id?: number;
   satId: number;
   subjectId: number;
   createdAt?: string;
   updatedAt?: string;
+}
+
+export class SubjectRegistration extends VersionedModel{
+  constructor(
+    satId: number,
+    subjectId: number,
+  ) {
+    super();
+  }
 }
