ab624f6af9f5a6e5f145303979a50f6749791117
Elroy Kanye <elroykanye@gmail.com>
2022-11-18 11:51:34 +0100

    setup component to manage school sections

--- src/app/modules/rc-dashboard/components/rc-settings/section/section.component.ts
+++ src/app/modules/rc-dashboard/components/rc-settings/section/section.component.ts
@@ -1,4 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Section} from "../../../../../models/dto/section.model";
+import {NO_ENTITY_ID} from "../../../../../models/base/base.model";
+import {SectionService} from "../../../../../services/section.service";
+import {School} from "../../../../../models/dto/school.model";
 
 @Component({
   selector: 'app-section',
@@ -6,11 +10,45 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./section.component.scss']
 })
 export class SectionComponent implements OnInit {
+  @Input() school?: School;
+  sections: Section[] = [];
 
-  constructor() {
+  constructor(
+    private sectionService: SectionService,
+  ) {
   }
 
   ngOnInit(): void {
   }
 
+  loadSections = () => {
+    if (this.school?.id) {
+      this.sectionService.getAllBySchoolId(this.school.id).subscribe((sections) => {
+        this.sections = sections
+      });
+    }
+  }
+
+  saveSection(sectionInput: HTMLInputElement | Section, blocked: boolean) {
+    if (this.school) {
+      if (blocked) {
+        if (sectionInput instanceof HTMLInputElement) {
+          if (sectionInput.hidden && sectionInput.value !== "") {
+            const section: Section = new Section(sectionInput.value, this.school.id!!);
+
+            this.sectionService.save(section).subscribe(() => {
+              sectionInput.value = "";
+              this.loadSections();
+            });
+          }
+        } else {
+          this.sectionService.update(sectionInput).subscribe(() => this.loadSections());
+        }
+      }
+    }
+  }
+
+  deleteSection(section: Section) {
+    this.sectionService.delete(section.id ?? NO_ENTITY_ID).subscribe(() => this.loadSections());
+  }
 }
